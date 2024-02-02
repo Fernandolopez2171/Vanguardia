d7611f60da3a1d2ff9192aec6238ea731db67a01
fernandolopez2171 <fernandolopez2171@unitec.edu>
2024-02-02 00:37:36 -0600

    new model

--- myApp/model/nameMongo.js
+++ myApp/model/nameMongo.js
@@ -4,10 +4,10 @@ const ExampleDataSchema = new mongoose.Schema({
   name: String,
 });
 
-const ExampleData = mongoose.model(
-  "ExampleData",
+const ExampleData = mongoose.model("example_datas", ExampleDataSchema);
+/*const ExampleData = mongoose.model(
+  "example_data",
   ExampleDataSchema,
   "example_data"
-);
-
+);*/
 module.exports = ExampleData;
